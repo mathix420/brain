6776efc1c2b6ccd2a40a735770048b4e9553de9e
Arnaud Gissinger <agissing@student.42.fr>
2024-02-29 19:17:04 +0100

    [UPDATE] toasts

--- components/Toasts.vue
+++ components/Toasts.vue
@@ -2,10 +2,10 @@
 const toasts = useToasts();
 const { dismiss } = useToast();
 const theme: Record<Toast["type"], string> = {
-  info: "bg-gray-900 border-gray-500 text-white",
-  success: "bg-green-900 border-green-500 text-white",
-  warning: "bg-yellow-900 border-yellow-500 text-white",
-  error: "bg-red-900 border-red-500 text-white",
+  info: "bg-gray-600/20 ring-white/30 text-white",
+  success: "bg-green-600/20 ring-white/30 text-white",
+  warning: "bg-yellow-600/20 ring-white/30 text-white",
+  error: "bg-red-600/20 ring-white/30 text-white",
 };
 
 // https://vercel.com/design/toast
@@ -23,7 +23,7 @@ const theme: Record<Toast["type"], string> = {
             :key="toast.uuid"
             :class="theme[toast.type]"
             role="alert"
-            class="absolute bottom-0 right-0 backdrop-blur-md transition-all ease-in-circ duration-300 transform second-last:-translate-y-[25%] second-last:scale-95 third-last:-translate-y-[50%] third-last:scale-90 fourth-last:-translate-y-[75%] fourth-last:scale-[85%] group-hover:second-last:-translate-y-[115%] group-hover:third-last:-translate-y-[230%] group-hover:fourth-last:-translate-y-[345%] before:top-full before:-bottom-full before:right-0 before:left-0 before:absolute group-hover:scale-100 flex items-center p-4 w-full max-w-sm border text-sm rounded-lg"
+            class="absolute shadow-xl shadow-black bottom-0 right-0 backdrop-blur-md transition-all ease-in-circ duration-300 transform second-last:-translate-y-[25%] second-last:scale-95 third-last:-translate-y-[50%] third-last:scale-90 fourth-last:-translate-y-[75%] fourth-last:scale-[85%] group-hover:second-last:-translate-y-[115%] group-hover:third-last:-translate-y-[230%] group-hover:fourth-last:-translate-y-[345%] before:top-full before:-bottom-full before:right-0 before:left-0 before:absolute group-hover:scale-100 flex items-center p-4 w-full max-w-sm ring-2 ring-inset text-sm rounded-lg"
           >
             <p>{{ toast.content }}</p>
             <div class="ms-auto">
@@ -79,7 +79,7 @@ const theme: Record<Toast["type"], string> = {
         --tw-scale-y: 85% !important;
         --tw-translate-y: -75% !important;
       }
-      &:nth-last-child(-n + 12) {
+      &:nth-last-child(-n + 2) {
         --tw-translate-y: 2.5rem /* 40px */ !important;
         --tw-scale-x: 1.1 !important;
         --tw-scale-y: 1.1 !important;
@@ -92,11 +92,6 @@ const theme: Record<Toast["type"], string> = {
         --tw-scale-x: 85% !important;
         --tw-scale-y: 85% !important;
         --tw-translate-y: -75% !important;
-      }
-      &:nth-last-child(-n + 1) {
-        --tw-translate-y: 2.5rem /* 40px */ !important;
-        --tw-scale-x: 1.1 !important;
-        --tw-scale-y: 1.1 !important;
       }
 
       opacity: 0 !important;
